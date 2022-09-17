a36d73bda54fffd20985e7f876643a95d25697bb
MaisaraCodes <108025035+MaisaraCodes@users.noreply.github.com>
2022-09-17 21:36:53 +0300

    update 3

--- Pages/js/script.js
+++ Pages/js/script.js
@@ -57,3 +57,13 @@ function showSuggestions(list){
     }
     suggBox.innerHTML = listData;
 }
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "275px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
